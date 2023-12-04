0b9254996a44ba3e2136a15543db6a3f3289409e
itsfeas <amf.fiaz@gmail.com>
2023-12-03 21:32:39 -0700

    modify base btn

--- src/components/common/btn/common-btn.tsx
+++ src/components/common/btn/common-btn.tsx
@@ -1,13 +1,26 @@
 import React, { ButtonHTMLAttributes } from 'react';
 
 interface BaseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    buttonType?: "submit" | "cancel" | "active" | "general",
 }
 
-const BaseButton: React.FC<BaseButtonProps> = ({ ...buttonProps }) => {
+const BaseButton: React.FC<BaseButtonProps> = ({ buttonType, ...buttonProps }) => {
+    const buttonStyle = (buttonType: string | null | undefined) => {
+        switch (buttonType) {
+            case "submit":
+                return "bg-green-500 hover:bg-green-700 text-white";
+            case "cancel":
+                return "bg-red-500 hover:bg-red-700 text-white";
+            case "active":
+                return "bg-blue-500 hover:bg-blue-700 text-white";
+            default:
+                return "bg-white hover:bg-gray-100 text-gray-800";
+        }
+    };
     return (
         <button
+            className={`p-4 ${buttonStyle(buttonType)} border border-gray-300 rounded cursor-pointer`}
             {...buttonProps}
-            className="p-4 bg-white border border-gray-300 rounded cursor-pointer"
         >
             {buttonProps.children}
         </button>
