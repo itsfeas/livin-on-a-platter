72ec465fb55dff7465e3c50541d4ad0e3e46e3ae
itsfeas <amf.fiaz@gmail.com>
2024-01-10 12:14:36 -0700

    api url modified

--- src/api/http-client/client.ts
+++ src/api/http-client/client.ts
@@ -2,7 +2,7 @@ interface RequestOptions {
     headers?: HeadersInit;
 }
 
-const BASE_URL = "http://localhost:8080/api/v1/";
+export const API_URL = "http://localhost:8080/api/v1";
 
 const handleResponse = async (response: Response) => {
     if (!response.ok) {
@@ -33,7 +33,7 @@ const del = async <T>(url: string, options?: RequestOptions): Promise<T> => {
 }
 
 export const httpClient = {
-    BASE_URL,
+    BASE_URL: API_URL,
     get,
     post,
     delete: del,
